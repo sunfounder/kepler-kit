845498763e81330bce2402a6e028f64365805826
SunFounder <support@sunfounder.com>
2023-07-31 18:11:54 +0800

    chang the utime to time

--- micropython/6.2_temperature_humidity.py
+++ micropython/6.2_temperature_humidity.py
@@ -4,13 +4,14 @@ from dht import DHT11, InvalidPulseCount
 
 pin = Pin(16, Pin.IN, Pin.PULL_UP)
 sensor = DHT11(pin)
+time.sleep(5)  # initial delay
 
 while True:
     try:
         sensor.measure()
         string = "Temperature:{}\nHumidity: {}".format(sensor.temperature, sensor.humidity)
         print(string)
-        utime.sleep(4)
+        time.sleep(4)
 
     except InvalidPulseCount as e:
         print('Bad pulse count - retrying ...')
