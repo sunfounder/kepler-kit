c10f963fea73436b544d49229eaa9bb8c83d5107
SunFounder <support@sunfounder.com>
2023-07-31 17:00:29 +0800

    Update 6.2_temperature_humidity.py
     Add a try/except block

--- micropython/6.2_temperature_humidity.py
+++ micropython/6.2_temperature_humidity.py
@@ -1,11 +1,16 @@
 from machine import Pin, I2C
 import utime as time
-from dht import DHT11
+from dht import DHT11, InvalidPulseCount
 
-pin = Pin(16, Pin.OUT, Pin.PULL_DOWN)
+pin = Pin(16, Pin.IN, Pin.PULL_UP)
 sensor = DHT11(pin)
 
 while True:
-    sensor.measure()
-    print("Temperature: {}, Humidity: {}".format(sensor.temperature, sensor.humidity))
-    time.sleep(1)
+    try:
+        sensor.measure()
+        string = "Temperature:{}\nHumidity: {}".format(sensor.temperature, sensor.humidity)
+        print(string)
+        utime.sleep(4)
+
+    except InvalidPulseCount as e:
+        print('Bad pulse count - retrying ...')
