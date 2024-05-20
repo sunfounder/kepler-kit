bec13537b88dc61c29aa462c97e957a307867241
Your Name$ <email@example.com>
2024-05-20 11:18:20 +0800

    join facebook group

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,6 +67,7 @@ html_css_files = [
 ]
 
 
+    
 # new
 rst_epilog = """
 .. |img_take_color| image:: /img/edit_colors.png
@@ -601,6 +602,10 @@ rst_epilog += """
 # open link in a new window
 
 rst_epilog += """
+
+.. |link_sf_facebook| raw:: html
+
+    <a href="https://bit.ly/44O1jYI" target="_blank">here</a>
 
 .. |link_german_tutorials| raw:: html
 
